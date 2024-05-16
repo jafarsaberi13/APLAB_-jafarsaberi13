fe2d5d84bbf79b277a3e867d024369052f2f49ab
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-05-16 13:28:09 +0330

    Delete Hospital_pro directory

--- Hospital_pro/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Hospital hospital = new Hospital();
-        Menu menu = new Menu(hospital);
-        menu.start();
-    }
-}
